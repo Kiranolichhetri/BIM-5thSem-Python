2f85300f1229505344c924e1bae05d569d0770b3
KIRAN OLI <kiranoli421@gmail.com>
2024-05-13 17:27:06 +0545

    Add files via upload

---
+++ Introduction/Comments.py
@@ -0,0 +1,18 @@
+# This is a single-line comment
+
+# You can write comments above code to explain its purpose
+x = 5  # Comments can also be written on the same line as code
+
+# Comments can be used to disable code
+# print("This won't be executed")
+
+"""
+This is a multi-line comment.
+You can use it to write longer comments
+that span multiple lines.
+"""
+
+# Comments are also useful for debugging
+# Add print statements to understand the flow of your program
+print("Hello")  # This will print "Hello"
+# print("World")  # This won't be executed
