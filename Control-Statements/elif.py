f2d5f87c8e945da1706c5655b70897d7c9ec440f
KIRAN OLI <kiranoli421@gmail.com>
2024-05-22 17:27:13 +0545

    Add files via upload

---
+++ Control-Statements/elif.py
@@ -0,0 +1,10 @@
+# Take input from the user
+num = int(input("Enter number: "))
+
+# Check if the number is negative, zero, or positive
+if num < 0:
+    print("Number is negative.")
+elif num == 0:
+    print("Number is zero.")
+else:
+    print("Number is positive.")
