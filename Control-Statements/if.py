9432c6e60eea0fc0b13ee930f4678b12599a8753
KIRAN OLI <kiranoli421@gmail.com>
2024-05-22 17:26:56 +0545

    Add files via upload

---
+++ Control-Statements/if.py
@@ -0,0 +1,18 @@
+# age that can drive 
+# conditional operator 
+# >, <, >=, <=, ==, !=
+
+age = int(input("Enter you age:"))
+print("your age is :",age)
+
+print(age<18)
+print(age<=18)
+print(age==18)
+print(age>=18)
+print(age!=18)
+
+
+if(age>18):
+    print("you can drive")
+else:
+    print("you cannot drive")    
