d700a04bab7d7e12f8d52c8fb96e8073166cf38a
KIRAN OLI <kiranoli421@gmail.com>
2024-05-29 19:02:09 +0545

    Add files via upload

---
+++ Solution/Qno9.py
@@ -0,0 +1,10 @@
+# Slice a list for last 5 items. Also slice first 4 items. 
+
+my_list = [1,2,3,4,5,6,7,8,9,10]
+
+last_five_items = my_list[-5:]
+
+first_four_items = my_list[:4]
+
+print("Last 5 items:",last_five_items)
+print("First 4 items:",first_four_items)
