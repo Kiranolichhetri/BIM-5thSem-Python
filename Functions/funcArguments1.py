0a7c06a415899b1cdff347022ce2a46d674e188f
KIRAN OLI <kiranoli421@gmail.com>
2024-06-06 22:12:35 +0545

    Add files via upload

---
+++ Functions/funcArguments1.py
@@ -0,0 +1,10 @@
+def average(a=9,b=1):
+    print("The average is",(a+b)/2)
+# average(4,6)
+average(b=9)
+
+
+def name(first_name,middle_name,last_name="oli"):
+    print("Hello,",first_name,middle_name,last_name)
+        
+name("kiran","bikram",)    
