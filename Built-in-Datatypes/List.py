94bd8dc623b6c2739590085d7ffbedbdc3380657
KIRAN OLI <kiranoli421@gmail.com>
2024-06-07 22:13:24 +0545

    Add files via upload

---
+++ Built-in-Datatypes/List.py
@@ -0,0 +1,22 @@
+my_list = [1, 2, 3, 4, 5]
+
+# Creating a list 
+print(my_list[0])  # Output: 1
+print(my_list[2])  # Output: 3
+
+# accessing list 
+print(my_list[0])  # Output: 1
+print(my_list[2])  # Output: 3
+
+# modifying list 
+my_list[1] = 10
+print(my_list)  # Output: [1, 10, 3, 4, 5]
+
+
+
+for item in my_list:
+    print(item)
+
+
+
+
