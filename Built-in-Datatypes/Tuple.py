bff8a4c054aaf0476952cbc8306481fdf204d286
KIRAN OLI <kiranoli421@gmail.com>
2024-06-08 11:03:23 +0545

    Add files via upload

---
+++ Built-in-Datatypes/Tuple.py
@@ -0,0 +1,20 @@
+#  a tuple is a collection of items that is ordered and immutable 
+# tuple is defined by enclosing '(,)' 
+
+
+my_tuple = (1, 2, 3, 4, 5)
+
+# single tuple
+
+single_tuple = (1,)
+
+
+# accessing elements 
+print(my_tuple[0])  
+print(my_tuple[2])  
+print(len(my_tuple))
+ 
+ 
+#  slicing tuple 
+print(my_tuple[1:4])  # Output: (2, 3, 4)
+
