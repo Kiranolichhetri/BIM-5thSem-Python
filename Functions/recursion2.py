11576c24f3914040d6554bfee529fe3d14d9f23c
Kiranolichhetri <kiranoli42@gmail.com>
2024-06-10 20:26:23 +0545

    recursion

---
+++ Functions/recursion2.py
@@ -0,0 +1,14 @@
+def fibonacci_recursive(n):
+
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+   
+    return fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2)
+
+
+print(fibonacci_recursive(0))  
+print(fibonacci_recursive(1))  
+print(fibonacci_recursive(5))  
+print(fibonacci_recursive(10)) 
