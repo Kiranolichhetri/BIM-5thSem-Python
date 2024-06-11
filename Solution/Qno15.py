48df4372d492f477e17e91599b5c5f3a4c99f432
Kiranolichhetri <kiranoli421@gmail.com>
2024-06-11 21:00:55 +0545

    empty set

---
+++ Solution/Qno15.py
@@ -0,0 +1,13 @@
+# create a emply sit, then add 5 items con it.
+# Create an empty list
+my_list = []
+
+# Add 5 items to the list
+my_list.append(1)
+my_list.append(2)
+my_list.append(3)
+my_list.append(4)
+my_list.append(5)
+
+# Print the list to show the items
+print("List after adding 5 items:", my_list)
