dcbec78ee37f0b6d2519b20b86b60a8a653b3edf
Kiranolichhetri <kiranoli421@gmail.com>
2024-06-12 20:50:49 +0545

    subset commit

--- Built-in-Datatypes/setsMethods2.py
+++ Built-in-Datatypes/setsMethods2.py
@@ -6,3 +6,20 @@ s3 = s1.union(s2)
 print(s3)  # Output: {1, 2, 3, 4, 5}
 
 
+# intersection(*others)
+s1 = {1, 2, 3}
+s2 = {3, 4, 5}
+s3 = s1.intersection(s2)
+print(s3)  # Output: {3}
+
+# difference(*others)
+s1 = {1, 2, 3}
+s2 = {3, 4, 5}
+s3 = s1.difference(s2)
+print(s3)  # Output: {1, 2}
+
+# symmetric_difference(other)
+s1 = {1, 2, 3}
+s2 = {3, 4, 5}
+s3 = s1.symmetric_difference(s2)
+print(s3)  # Output: {1, 2, 4, 5}
