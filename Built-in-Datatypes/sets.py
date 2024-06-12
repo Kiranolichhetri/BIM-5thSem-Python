fe3d4276bdeb580854068716db3bad42ca9dbb9f
Kiranolichhetri <kiranoli421@gmail.com>
2024-06-12 20:31:34 +0545

    sets commit

---
+++ Built-in-Datatypes/sets.py
@@ -0,0 +1,11 @@
+# Using curly braces
+my_set = {1, 2, 3, 4, 5}
+print(my_set)  # Output: {1, 2, 3, 4, 5}
+
+# Using the set() function
+another_set = set([1, 2, 3, 4, 5])
+print(another_set)  # Output: {1, 2, 3, 4, 5}
+
+# Creating an empty set
+empty_set = set()
+print(empty_set)  # Output: set()
