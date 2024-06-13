38d88e14885a2bd745538018b557e115f96a0106
Kiranolichhetri <kiranoli421@gmail.com>
2024-06-13 19:33:20 +0545

    commit

--- Built-in-Datatypes/DictMethods2.py
+++ Built-in-Datatypes/DictMethods2.py
@@ -17,3 +17,13 @@ email = my_dict.setdefault('email', 'john@example.com')
 print(email)  # Output: john@example.com
 print(my_dict)  # Output: {'name': 'John', 'age': 25, 'email': 'john@example.com'}
 
+# update 
+my_dict = {'name': 'John', 'age': 25}
+new_info = {'age': 26, 'city': 'New York'}
+my_dict.update(new_info)
+print(my_dict)  # Output: {'name': 'John', 'age': 26, 'city': 'New York'}
+
+# values 
+my_dict = {'name': 'John', 'age': 25, 'city': 'New York'}
+print(my_dict.values())  # Output: dict_values(['John', 25, 'New York'])
+
