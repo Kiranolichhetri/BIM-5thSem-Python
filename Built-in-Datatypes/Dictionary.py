a670004b854297feea38463a1cc5ce80f2f0c8fb
Kiranolichhetri <kiranoli421@gmail.com>
2024-06-13 19:20:16 +0545

    commit Dictionary

---
+++ Built-in-Datatypes/Dictionary.py
@@ -0,0 +1,16 @@
+# Using curly braces
+my_dict = {
+    'name': 'John',
+    'age': 25,
+    'city': 'New York'
+}
+
+# Using the dict() function
+my_dicti = dict(name='kiran', age=25, city='Nepalgunj')
+
+
+print(my_dict['name'])  # Output: John
+print(my_dict.get('age'))  # Output: 25
+
+print(my_dicti['name'])  # Output: John
+print(my_dicti.get('age'))  # Output: 25
