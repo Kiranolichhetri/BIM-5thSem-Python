1a96df41cd9c05c1a24f97efb8deb480bdca7806
Kiranolichhetri <kiranoli421@gmail.com>
2024-06-17 20:30:54 +0545

    commit

---
+++ Exercise/Exercise-3.py
@@ -0,0 +1,6 @@
+# Create a program capable of displaying questions to the user like KBC.
+# Use List data type to store the questions and their correct answer
+# Display the final amout the person is takig home after playing the game.
+
+
+
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # BIM-5thSem-Python
+<h1>Python Programming Questions and Solutions</h1>
+<p>
